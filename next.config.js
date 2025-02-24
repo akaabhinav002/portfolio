096f69fd18196dc9fbf464ff3d921dddb4a0634f
akaabhinav002 <abhinavkangale123@gmail.com>
2025-02-24 22:18:04 +0530

    Fixed Next.js build and deployment issues

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone", // Ensures Next.js can generate proper build files
-  experimental: {
-    appDir: true, // Ensure this is enabled if using Next.js 13+
-  },
+ 
 };
 
 module.exports = nextConfig;
