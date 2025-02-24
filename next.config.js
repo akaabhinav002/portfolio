0244f64de4c4458ba2d042b88b466dfc33bfdcef
akaabhinav002 <abhinavkangale123@gmail.com>
2025-02-24 22:05:06 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: "standalone", // Ensures Next.js can generate proper build files
+  experimental: {
+    appDir: true, // Ensure this is enabled if using Next.js 13+
+  },
 };
 
 module.exports = nextConfig;
